55b417a5403183c37adfd0b914d7b232d2e7fd59
凌风1 <429212378@qq.com>
2022-06-12 14:41:04 +0800

    Merge branch 'master' of https://gitee.com/yoyounger/nft
    # Conflicts:
    #	public/nginx.htaccess

---
+++ application/admin/lang/zh-cn/shopro/box/box.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'Id'                     => 'ID',
+    'Category_id'            => '分类ID',
+    'Box_name'               => '盲盒名称',
+    'Box_banner_images'      => '盲盒详情banner',
+    'Box_banner_images_desc' => '盲盒详情banner文字',
+    'Coin_price'             => '金币价格',
+    'Is_hot'                 => '热门标识:1热门,0非热门',
+    'Is_cheap'               => '低价标识:1低价,0非低价',
+    'Is_try'                 => '试一试标识',
+    'Sales'                  => '销量',
+    'Create_time'            => '创建时间',
+    'Update_time'            => '更新时间',
+    'Delete_time'            => '删除时间',
+    'Sort'                   => '自定义排序',
+    'Switch'                 => '显示隐藏'
+];
---
+++ application/admin/lang/zh-cn/shopro/box/detail.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Id'          => 'ID',
+    'Box_id'      => '盲盒ID',
+    'Goods_id'    => '商品ID',
+    'Rate'        => '概率',
+    'Weigh'       => '权重',
+    'Create_time' => '创建时间',
+    'Update_time' => '更新时间',
+    'Delete_time' => '删除时间'
+];
